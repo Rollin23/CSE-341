2e68ecf154c037c3c149549c8ce020c51e7a42cb
Stephen Rollins <rollin23@byui.edu>
2026-04-25 13:00:35 -0600

    routes

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require("express");
-
 const app = express();
-const lesson1Controller = require('./controllers/lesson1')
-
 const PORT = process.env.PORT || 3000;
 
-app.get("/", lesson1Controller.stephenRoute)
-
-app.get("/leroy", lesson1Controller.bobRoute)
+app.use('/', require('./routes'))
 
 app.listen(PORT, () => {
   console.log('Web Server is listening at port ' + PORT);
